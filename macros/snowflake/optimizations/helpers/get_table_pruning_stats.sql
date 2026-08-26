89eb402e48cf2ea34b1432ad20b48ba3736423bc
Jessica Stayton <jessica.stayton@dbtlabs.com>
2026-08-26 15:08:18 -0500

    fixed bug

--- macros/snowflake/optimizations/helpers/get_table_pruning_stats.sql
+++ macros/snowflake/optimizations/helpers/get_table_pruning_stats.sql
@@ -14,12 +14,12 @@
         coalesce(sum(num_queries), 0) as total_query_count,
         case
             when sum(num_queries) > 0
-            then sum(query_execution_time_ms) / sum(num_queries)
+            then sum(aggregate_query_execution_time) / sum(num_queries)
             else 0
         end as avg_execution_time_ms
     from snowflake.account_usage.table_query_pruning_history
-    where table_database = '{{ database_name }}'
-      and table_schema = '{{ schema_name }}'
+    where database_name = '{{ database_name }}'
+      and schema_name = '{{ schema_name }}'
       and table_name = '{{ table_name }}'
       and interval_start_time >= dateadd(day, -{{ lookback_days }}, current_timestamp())
   {% endset %}
